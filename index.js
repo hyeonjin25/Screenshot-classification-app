d69a3d6486e48f512d438ced1d4ebbd288051f3f
hyeonjin25 <shj41738866@gmail.com>
2023-04-30 21:56:28 +0900

    react-native-paper 설치

--- index.js
+++ index.js
@@ -2,8 +2,27 @@
  * @format
  */
 
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {name as appName} from './app.json';
+import App from './App';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
