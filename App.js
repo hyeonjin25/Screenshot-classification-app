b7ff9a458f80b6da610f6804b73b85218f5485be
hyeonjin25 <shj41738866@gmail.com>
2023-04-30 21:57:03 +0900

    사진 전송 api 연결

--- App.js
+++ App.js
@@ -5,51 +5,67 @@
  * @format
  */
 
+import {NavigationContainer} from '@react-navigation/native';
+import axios from 'axios';
 import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-} from 'react-native';
-
+import {BASE_URL} from './src/config/api';
+import {StackNavigator} from './src/navigator/StackNavigator';
 
 function App() {
-
-  const backgroundStyle = {
-    backgroundColor: 'beige',
-    flex:1
-  };
-
   const RNFS = require('react-native-fs');
 
-  RNFS.readDir(RNFS.ExternalStorageDirectoryPath+'/DCIM/Screenshots' ) 
-  .then((result) => {
-    console.log('GOT RESULT', result);
+  RNFS.readDir(RNFS.ExternalStorageDirectoryPath + '/DCIM/Screenshots')
+    .then(result => {
+      // console.log('GOT RESULT', result);
+      sendImages(result);
+      return result.map(res => [RNFS.stat(res), res]);
+    })
+    .then(statResult => {
+      // console.log('statResult', statResult);
+      if (statResult[0].isFile()) {
+        // if we have a file, read it
+        return RNFS.readFile(statResult[0]);
+      }
 
-    return result.map((res)=>(
-      [RNFS.stat(res.path), res.path]
-    )) 
-  })
-  .then((statResult) => {
-    console.log('statResult', statResult);
-    if (statResult[0].isFile()) {
-      // if we have a file, read it
-      return RNFS.readFile(statResult[0]);
-    }
+      return 'no file';
+    })
+    .then(contents => {
+      // log the file contents
+      console.log(contents);
+    })
+    .catch(err => {
+      console.log(err.message, err.code);
+    });
 
-    return 'no file';
-  })
-  .then((contents) => {
-    // log the file contents
-    console.log(contents);
-  })
-  .catch((err) => {
-    console.log(err.message, err.code);
-  });
+  const sendImages = images => {
+    const formData = new FormData();
+    console.log('콘솔', images);
+    formData.append('userDeviceToken', 'ddd');
+
+    images?.map(image => {
+      formData.append('images', {
+        uri: 'file://' + image.path,
+        name: image.name,
+        type: 'image/jpeg',
+      });
+    });
+
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+
+    axios
+      .post(`${BASE_URL}/images`, formData, config)
+      .then(data => console.log(data))
+      .catch(err => console.log('/images 에러 : ', err));
+  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <Text>안농</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
