e7facbd4fd7323c4ddd6a025d3bc8d7d6782e94c
hyeonjin25 <shj41738866@gmail.com>
2023-05-04 15:48:27 +0900

    검색 바 적용, 전체 태그 navigate page 변경

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components';
 import {Text} from 'react-native-paper';
 import {Tag} from './components/List/Tag';
-import {useNavigation} from '@react-navigation/native';
+import {
+  useFocusEffect,
+  useIsFocused,
+  useNavigation,
+} from '@react-navigation/native';
+import {SearchBar} from './components/bar/SearchBar';
+import {useSetRecoilState} from 'recoil';
+import {QueryState} from './state/QueryState';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const setQueryState = useSetRecoilState(QueryState);
+
+  useFocusEffect(() => {
+    setQueryState('');
+  }, []);
 
   return (
     <>
       <Container>
+        <SearchBar />
         <TitleBox>
           <Text variant="headlineSmall" style={{fontWeight: 'bold'}}>
             즐겨 찾는 태그
@@ -36,7 +49,7 @@ const HomeScreen = () => {
           <Text
             variant="titleMedium"
             onPress={() => {
-              navigation.navigate('Tag', 'favorite');
+              navigation.navigate('Tag', 'whole');
             }}
             style={{color: 'gray'}}>
             전체보기
