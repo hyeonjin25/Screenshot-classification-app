28edd187338a89fda2b199c61dff3e482ba8e622
hyeonjin25 <shj41738866@gmail.com>
2023-05-04 15:46:50 +0900

    add search bar

---
+++ src/components/bar/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import {useNavigation} from '@react-navigation/native';
+import {Searchbar} from 'react-native-paper';
+import {QueryState} from '../../state/QueryState';
+import {useRecoilState} from 'recoil';
+
+export const SearchBar = () => {
+  const navigation = useNavigation();
+  const [queryState, setQueryState] = useRecoilState(QueryState);
+
+  return (
+    <Container>
+      <Searchbar
+        Icon="mdiMagnify"
+        placeholder="원하는 태그를 입력하세요"
+        onChangeText={setQueryState}
+        value={queryState}
+        onSubmitEditing={() => {
+          navigation.navigate('Search');
+        }}
+      />
+    </Container>
+  );
+};
+
+const Container = styled.View``;
