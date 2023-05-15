5400e25de20207a936db9b6d2c16ace9fbaa5bf2
hyeonjin25 <shj41738866@gmail.com>
2023-05-15 15:26:03 +0900

    change backButton

--- src/components/bar/BackBar.js
+++ src/components/bar/BackBar.js
@@ -1,15 +1,26 @@
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+import {Header} from '@rneui/themed';
+import {Icon} from '@rneui/themed/dist';
 import React from 'react';
-import {Appbar} from 'react-native-paper';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {AppColor} from '../../utils/GlobalStyles';
 
 const BackBar = () => {
   const navigation = useNavigation();
   return (
     <>
-      <Appbar.BackAction
-        onPress={() => {
-          navigation.pop();
-        }}
+      <Header
+        leftComponent={
+          <TouchableOpacity
+            style={{marginLeft: 10}}
+            onPress={() => {
+              navigation.pop();
+              console.log('뒤로');
+            }}>
+            <Icon type="ionicon" name="chevron-back" />
+          </TouchableOpacity>
+        }
+        backgroundColor={AppColor.white}
       />
     </>
   );
