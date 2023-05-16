10bb763839523d42ec2a4cc62f2860d5a7da09b3
hyeonjin25 <shj41738866@gmail.com>
2023-05-16 12:42:47 +0900

    STYLE : Change title position

--- src/TagImagesScreen.js
+++ src/TagImagesScreen.js
@@ -4,36 +4,15 @@ import BackBar from './components/bar/BackBar';
 import {ImageList} from './components/List/ImageList';
 import {Text} from '@rneui/themed/dist';
 
-const imageURI = [
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-  'http://placeimg.com/640/480/animals',
-];
-
 const TagImagesScreen = props => {
-  console.log(props)
+  console.log(props);
   return (
     <>
-      <BackBar />
+      <BackBar title={`# ${props.route.params}`} />
       <Container>
         <TitleBox>
-          <Text h4># {props.route.params}</Text>
         </TitleBox>
-        <ImageList data={imageURI} />
+        <ImageList />
       </Container>
     </>
   );
